32ea5f34b7074f8cf618ae0b948e05645f1b0407
Seth Hall <seth@corelight.com>
2017-08-15 11:01:46 -0400

    Initial commit.
    Moved over from the old bro-plugins repository.

---
+++ cmake/FindNetmap.cmake
@@ -0,0 +1,41 @@
+# THIS ISN'T CURRENTLY USED!!!  We are shipping the netmap
+# header files with this distribution for now.
+#
+#
+#
+#
+#
+# - Try to find netmap includes.
+#
+#
+# Variables used by this module, they can change the default behaviour and need
+# to be set before calling find_package:
+#
+#  NETMAP_ROOT_DIR           Set this variable to the root installation of
+#                            netmap if the module has problems finding the
+#                            proper installation path.
+#
+# Variables defined by this module:
+#
+#  NETMAP_FOUND              System has netmap API files.
+#  NETMAP_INCLUDE_DIR        The netmap include directory.
+
+find_path(NETMAP_ROOT_DIR
+    NAMES include/net/netmap_user.h
+    HINTS /usr/ /usr/local/
+)
+
+find_path(NETMAP_INCLUDE_DIR
+    NAMES net/netmap_user.h
+    HINTS ${NETMAP_ROOT_DIR}/include
+)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Netmap DEFAULT_MSG
+    NETMAP_INCLUDE_DIR
+)
+
+mark_as_advanced(
+    NETMAP_ROOT_DIR
+    NETMAP_INCLUDE_DIR
+)
